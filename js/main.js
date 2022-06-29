e4436debc9f41aa2a2b971545acb71574074c7bd
fosa-ij <efosaij@gmail.com>
2022-06-29 21:21:34 +0100

    code the menu btn functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+const menuBar = document.querySelector('.hidden-bar')
+const toggleUl = document.querySelector('.ul')
+const bookUL = document.querySelector('.bookkk')
 
+menuBar.addEventListener('click', _ => {
+    toggleUl.classList.toggle('show-menu')
+    bookUL.classList.remove('bookkk')
+    bookUL.classList.remove('ul')
+})
